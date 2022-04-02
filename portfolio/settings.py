bfdd8ed978cc896e5a37d0af22f3043832b75b8a
Adedotun Adepoju <d.e.adepoju@gmail.com>
2022-04-02 23:08:54 +0100

    latest attempt

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-PROJECT_ROOT   =   os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
+PROJECT_ROOT   =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = (
