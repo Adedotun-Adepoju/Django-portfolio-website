a92abd3ba84a191dfb1b9ed67fb6e15e819ab7c1
Adedotun Adepoju <d.e.adepoju@gmail.com>
2022-04-03 13:03:28 +0100

    style the background image with html rather than css

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9sth1%wde4fn6xfi(-mjm3^_yvk5(lm^k8y&jw7hh$nm=_lk8s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["dotun-portfolio.herokuapp.com", "127.0.0.1"]
 
@@ -125,12 +125,12 @@ USE_TZ = True
 PROJECT_ROOT   =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+#STATICFILES_DIRS = (
+    #os.path.join(PROJECT_ROOT, 'static'),
+#)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
